e5c99f6c6c4b252a1a0e9a0a02b052e20644a3b4
ks9987 <kengo.suzuki@relic.co.jp>
2023-06-27 04:10:08 +0900

    Deploy

--- manifest.ts
+++ manifest.ts
@@ -7,10 +7,10 @@ import GreetingWorkflow from "./workflows/greeting_workflow.ts";
  * https://api.slack.com/automation/manifest
  */
 export default Manifest({
-  name: "data-parrot",
+  name: "DataParrot",
   description:
     "A sample that demonstrates using a function, workflow and trigger to send a greeting",
-  icon: "assets/default_new_app_icon.png",
+  icon: "assets/bunnyparrot.png",
   workflows: [GreetingWorkflow],
   outgoingDomains: [],
   botScopes: ["commands", "chat:write", "chat:write.public"],
